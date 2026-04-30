511673db92480bdd757709136aa345906a4c4cb2
snodir <nodirbeksadullayev512@outlook.com>
2026-04-30 17:02:49 +0500

    Power Shell scripts to run SSIS packages

---
+++ PowerShellProjects/LoadFromMultipleFlatFilesToSqlTable.ps1
@@ -0,0 +1,15 @@
+﻿Set-ExecutionPolicy remoteSigned
+Import-Module SQLPS
+Invoke-Sqlcmd "insert into CustomerAllAtOnceThroughPowerShell values('Jack', 'Stuttgart')" -ServerInstance 'DESKTOP-20RAN2E'  -Database 'SSISwithBek'
+
+$file_array = Get-ChildItem -Path d:\source\Flat_subsource\
+foreach($each_file in $file_array)
+{
+    $cust_text = Import-Csv -Path $each_file.FullName
+    foreach($each_row in $cust_text)
+    {
+        $name = $each_row.name
+        $city = $each_row.city
+        Invoke-Sqlcmd -Query "insert into CustomerAllAtOnceThroughPowerShell values('$name', '$city')" -ServerInstance 'DESKTOP-20RAN2E' -Database 'SSISwithBek'
+    }
+}
